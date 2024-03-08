c666331cab80013f6dbb0d91dd854ef18e510c6e
pe4enka22 <missis.bug@gmail.com>
2024-03-08 16:01:04 +0200

    change url

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://jsonplaceholder.typicode.com',
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
